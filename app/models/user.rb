ea868859a0d53ca732ce1ca50d1d8ea5b19bed59
Ethann <ethann@shkermit.ch>
2025-01-30 09:48:07 +0100

    feat: Add dynamic nutrient calculation methods for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,11 @@ class User < ApplicationRecord
 
   validates :firstname, presence: true
   validates :lastname, presence: true
+
+  %i[calories fat carbs protein].each do |nutrient|
+    define_method("#{nutrient}_of_the_days") do |date = Date.today|
+      meals = Meal.by_date_and_user(date, self)
+      meals.sum(nutrient)
+    end
+  end
 end
