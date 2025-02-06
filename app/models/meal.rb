d41bfa4b4093229a03d28fdbfb9abcaa781a6de0
Ethann <ethann@shkermit.ch>
2025-02-06 09:36:10 +0100

    feat: Add picture attachment to Meal model

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -3,8 +3,9 @@ class Meal < ApplicationRecord
   validates :meal_type, inclusion: { in: 0..3 }
 
   belongs_to :user
+  has_one_attached :picture
 
   def self.by_date_and_user(date, user)
-    where(date: date, user: user)
+   where(date: date, user: user)
   end
 end
