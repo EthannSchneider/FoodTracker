786a92207f0b12668a4413af3f7e7bc56913726a
Ethann <ethann@shkermit.ch>
2025-02-06 09:45:12 +0100

    feat: Add picture attachment to BodyMeasurement model

--- app/models/body_measurement.rb
+++ app/models/body_measurement.rb
@@ -7,6 +7,8 @@ class BodyMeasurement < ApplicationRecord
   validates :waist_size, :chest_size, :hip_size, :left_arm_size, :right_arm_size, :left_forearm_size, :right_forearm_size, :right_thighs_size, :left_thighs_size, :right_calves_size, :left_calves_size, numericality: { greater_than: 0 }, allow_nil: true
   validates_uniqueness_of :date, scope: :user_id
 
+  has_one_attached :picture
+
   def self.by_date_and_user(date, user)
     find_by(date: date, user: user)
   end
